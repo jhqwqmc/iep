47d9cace991abfb9af7c99e95d86a99640fa2a4a
Efnilite <35348263+Efnilite@users.noreply.github.com>
2024-03-08 13:53:09 +0100

    Added /iep schematic

--- src/main/kotlin/dev/efnilite/iep/Command.kt
+++ src/main/kotlin/dev/efnilite/iep/Command.kt
@@ -9,10 +9,16 @@ import dev.efnilite.iep.menu.PlayMenu
 import dev.efnilite.iep.menu.SettingsMenu
 import dev.efnilite.iep.player.ElytraPlayer.Companion.asElytraPlayer
 import dev.efnilite.vilib.command.ViCommand
+import dev.efnilite.vilib.schematic.Schematic
 import dev.efnilite.vilib.util.Cooldowns
+import dev.efnilite.vilib.util.Locations
 import dev.efnilite.vilib.util.Strings
+import org.bukkit.Location
 import org.bukkit.command.CommandSender
 import org.bukkit.entity.Player
+import org.bukkit.util.Vector
+import java.io.File
+import java.util.*
 
 object Command : ViCommand() {
 
@@ -28,6 +34,12 @@ object Command : ViCommand() {
                 send("<#7700FF>/iep leave <dark_gray>- <gray>Leaves the game")
                 send("<#7700FF>/iep seed <seed> <dark_gray>- <gray>Set the current seed")
                 send("")
+
+                if (sender.isOp) {
+                    send("<#7700FF>/iep schematic <pos1> <pos2> <dark_gray>- <gray>Saves the area between the positions")
+                    send("<gray>⋅ Example: /iep schematic 0,0,0 -10,-10,-10")
+                    send("")
+                }
             }
 
             return true
@@ -93,6 +105,22 @@ object Command : ViCommand() {
                     } catch (ex: NumberFormatException) {
                         iep.send(Locales.getString(sender, "settings.seed.invalid").replace("%a", args[1]))
                     }
+                }
+                "schematic" -> {
+                    if (!sender.isOp) return true
+
+                    val pos1Nums = args[1].split(",").map { it.toInt() }
+                    val pos2Nums = args[2].split(",").map { it.toInt() }
+
+                    val pos1 = Vector(pos1Nums[0], pos1Nums[1], pos1Nums[2]).toLocation(sender.world)
+                    val pos2 = Vector(pos2Nums[0], pos2Nums[1], pos2Nums[2]).toLocation(sender.world)
+
+                    val uuid = UUID.randomUUID()
+                    val file = File(IEP.instance.dataFolder, "schematics/$uuid")
+
+                    sender.send("<gray>Saving your schematic as $uuid...")
+
+                    Schematic.create().save(file, pos1, pos2, IEP.instance)
                 }
             }
         }
